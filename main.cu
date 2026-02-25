34548c79bad17c9f7d2cb390f85a640373f618a5
gusrlLee <gusrl3204@naver.com>
2026-02-25 21:10:41 +0900

    external library setting done

--- main.cu
+++ main.cu
@@ -1,11 +1,20 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <cuda_gl_interop.h>
 #include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
     glfwInit();
-
     glfwTerminate();
     return 0;
 }
+
